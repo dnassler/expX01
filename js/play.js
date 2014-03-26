19810f7b133ada5a5604b17bb10e51eb439cbf88
Derek Nassler <dnassler@rogers.com>
2014-03-25 20:48:41 -0400

    modified drawing to allow for undo and to use less than 3 fingers to draw

--- js/play.js
+++ js/play.js
@@ -1,6 +1,8 @@
 var play_state = function(game) {
 	that = this;
 
+	this.debug = false;
+
 	this.p1 = null;
 	this.p2 = null;
 	this.p3 = null;
@@ -18,6 +20,16 @@ var play_state = function(game) {
 		"rgb(112,128,144)","rgb(106,90,205)","rgb(255,215,0)","rgb(25,25,112)","rgb(72,61,139)",
 		"rgb(240,128,128)","rgb(188,143,143)","rgb(30,144,255)","rgb(220,20,60)","rgb(47,79,79)"
 		];
+
+	this.doubleTapOccurred = false;
+
+	this.shapeHistory = [];
+
+	this.drawingShapeStartTime = 0;
+	this.saveNewShapeThreshold = 1000;
+
+	this.swipeOccurredAt = 0;
+	this.swipeOccurred = false;
 };
 play_state.prototype = {
 
@@ -59,6 +71,8 @@ play_state.prototype = {
     this.label1 = this.game.add.text(this.game.world.width/2,this.game.world.height/2,"Random text in the play state.", style);
     this.label1.anchor.setTo(0.5,0.5);
 
+    this.rt0 = this.game.add.renderTexture(this.game.world.width, this.game.world.height);
+    this.rt0Sprite = this.game.make.sprite(0,0,this.rt0);
     this.rt1 = this.game.add.renderTexture(this.game.world.width, this.game.world.height);
     this.renderedShapes = this.game.add.sprite(0,0,this.rt1);
 
@@ -80,7 +94,8 @@ play_state.prototype = {
 		// restart game when necessary?
 		// this.restart();
 
-		if ( this.p1.isDown && this.p2.isDown && this.p3.isDown ) {
+		//if ( this.p1.isDown && this.p2.isDown && this.p3.isDown ) {
+		if ( this.p1 && this.p1.isDown && this.p2 && this.p3 && this.p2.screenX > -1 && this.p3.screenY > -1 ) {
 			//this.g1.beginFill("#87CEFA");
 			this.g1.clear();
 			var ctx = this.g1.ctx;
@@ -91,14 +106,35 @@ play_state.prototype = {
 			ctx.lineTo( this.p3.screenX, this.p3.screenY );
 			ctx.closePath();
 			ctx.fill();
+
+			if ( !this.drawingShape ) {
+				this.drawingShapeStartTime = this.game.time.now;
+			}
 			this.drawingShape = true;
 			//ctx.fillRect(this.p1.screenX, this.p1.screenY, (this.p2.screenX - this.p1.screenX), (this.p2.screenY - this.p1.screenY));
+		
 		} else {
 			this.g1.clear();
 		}
+
+		if ( this.doubleTapOccurred && (this.game.time.now - this.p1.previousTapTime) > 1000 ) {
+    	this.doubleTapOccurred = false;
+    }
+    
+    if ( this.game.time.now - this.swipeOccurredAt > 1000 ) {
+    	this.swipeOccurred = false;
+    }
+    if ( !this.swipeOccurred && this.isSwipe() ) {
+    	this.swipeOccurred = true;
+    	this.swipeOccurredAt = this.game.time.now;
+    }
+
 	},
 
 	render: function() {
+
+		if ( !this.debug ) return;
+
 		//game.debug.text("w="+w+", h="+h+", window.devicePixelRatio="+window.devicePixelRatio,10,10);
 		this.game.debug.text("window.innerWidth="+window.innerWidth+", window.innerHeight="+window.innerHeight,10,30);
     this.game.debug.text("game.width="+this.game.width+", game.height="+this.game.height,10,50);
@@ -108,12 +144,24 @@ play_state.prototype = {
     if ( this.p2 ) this.game.debug.text("p2.x="+this.p2.screenX+", p2.y="+this.p2.screenY+", p2.isDown="+this.p2.isDown, 10,120);
     if ( this.p3 ) this.game.debug.text("p3.x="+this.p3.screenX+", p3.y="+this.p3.screenY+", p3.isDown="+this.p3.isDown, 10,140);
     if ( this.p4 ) this.game.debug.text("p4.x="+this.p4.screenX+", p4.y="+this.p4.screenY+", p4.isDown="+this.p4.isDown, 10,160);
+    if ( this.game.input.activePointer ) {
+    		
+    		this.game.debug.text("activePointer.isDown="+this.game.input.activePointer.isDown
+    			+", distanceDown="+(Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown))
+    			+", timeUp-timeDown="+(this.game.input.activePointer.timeUp - this.game.input.activePointer.timeDown)
+    			, 10,200);
+    	}
 
     this.game.debug.pointer(this.p1);
     this.game.debug.pointer(this.p2);
     this.game.debug.pointer(this.p3);
     this.game.debug.pointer(this.p4);
 
+    if ( this.doubleTapOccurred ) {
+    	this.game.debug.text("p1 was double clicked: p1.msSinceLastClick="+this.p1.msSinceLastClick, 10,180);
+    } else if ( (this.game.time.now - this.swipeOccurredAt) < 1000 ) {
+    	this.game.debug.text("swipe occurred! this.game.input.activePointer.isDown="+this.game.input.activePointer.isDown+" swipetime="+this.swipeOccurredAt, 10, 180);
+    }
 	},
 
 	restart: function() {
@@ -136,6 +184,14 @@ play_state.prototype = {
 		console.log("clickListener: x="+x);
 		console.log("clickListener: y="+y);
 
+		if ( that.p1.msSinceLastClick <= that.game.input.doubleTapRate ) {
+			that.doubleTapOccurred = true;
+			that.rt1.renderXY( that.rt0Sprite, 0,0, true ); // undo the last shape addition
+		}
+		//if ( p1.totalTouches == 2 )	{
+		//	that.game.debug.text("clickListener: p1.totalTouches="+that.p1.totalTouches, 10,220);
+		//}	
+
 		// console.log("clickListener: that.game.input.pointer1.x="+that.game.input.pointer1.x);
 		// console.log("clickListener: that.game.input.pointer1.y="+that.game.input.pointer1.y);
 
@@ -155,23 +211,44 @@ play_state.prototype = {
 			// commit the shape
 			that.drawingShape = false;
 
-			that.newShapeBM.clear();
-			var ctx = that.newShapeBM.ctx;
-			ctx.save();
-			//ctx.fillStyle = "#708090";
-			ctx.fillStyle = Phaser.Math.getRandom( that.shapeColorArr );
-			ctx.beginPath();
-			ctx.moveTo( that.p1.screenX, that.p1.screenY );
-			ctx.lineTo( that.p2.screenX, that.p2.screenY );
-			ctx.lineTo( that.p3.screenX, that.p3.screenY );
-			ctx.closePath();
-			ctx.globalAlpha = 0.4;
-			ctx.fill();
-			ctx.restore();
-			//this.newShapeSprite.visible = true;
-			that.rt1.renderXY( that.newShapeSprite, 0,0, false );
-			//this.newShapeSprite.visible = false;
+			if ( (that.game.time.now - that.drawingShapeStartTime) > that.saveNewShapeThreshold ) {
+
+				that.newShapeBM.clear();
+				var ctx = that.newShapeBM.ctx;
+				ctx.save();
+				//ctx.fillStyle = "#708090";
+				ctx.fillStyle = Phaser.Math.getRandom( that.shapeColorArr );
+				ctx.beginPath();
+				ctx.moveTo( that.p1.screenX, that.p1.screenY );
+				ctx.lineTo( that.p2.screenX, that.p2.screenY );
+				ctx.lineTo( that.p3.screenX, that.p3.screenY );
+				ctx.closePath();
+				ctx.globalAlpha = 0.4;
+				ctx.fill();
+				ctx.restore();
+				//this.newShapeSprite.visible = true;
+				that.rt0.renderXY( that.renderedShapes, 0,0, true ); // save the state of the drawn shapes before adding another
+				that.rt1.renderXY( that.newShapeSprite, 0,0, false );
+				//this.newShapeSprite.visible = false;
+
+				//var newShapeInfo = {p1:{x:p1.screenX, y:p1.screenY}, p2:{x:p2.screenX, y:p2.screenY}, p3:{x:p3.screenX, y:p3.screenY}};
+				//that.shapeHistory.push( newShapeInfo );
+				//that.lastShape = newShapeInfo;
+
+			}
+			
 		}
+	},
+
+	isSwipe: function() {
+		// 1st parameter determines the distance of the active pointer. My swipe distance trashhold is 150, you can play around with this value
+		// in order to get a better feeling.
+		// So basicly what you do is look for a certain distance (150) in a given time frame (min 100ms till 250ms).
+	  if ( this.game.input.activePointer.isDown ) return false;
+	  if ( !this.game.input.activePointer.justReleased() ) return false;
+	  var durDown = (this.game.input.activePointer.timeUp - this.game.input.activePointer.timeDown);
+	  return (Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown) > 150 && durDown > 100 && durDown < 250);
 	}
+
 
 };
